b085306097e77364fb1a6c34a6d5ac1d05785560
Victor Goubet <victor.goubet@edu.devinci.fr>
2021-03-15 17:44:09 +0100

    set start

--- server/scrap/scrapper.js
+++ server/scrap/scrapper.js
@@ -1,31 +1,31 @@
 const axios = require('axios');
   
-  const get_data = async url =>{
-    let response = await axios(url);
-    const {data, status} = response;
-    if (status >= 200 && status < 300) {
-      return data
-    }
-    return null
+const get_data = async url =>{
+  let response = await axios(url);
+  const {data, status} = response;
+  if (status >= 200 && status < 300) {
+    return data
   }
+  return null
+}
   
-  const scrape = async shop => {
-    const shoptools = require('./'+shop.brand.toLowerCase().replace(' ', ''))
-    let data = await get_data(shop.link)
+const scrape = async shop => {
+  const shoptools = require('./'+shop.brand.toLowerCase().replace(' ', ''))
+  let data = await get_data(shop.link)
 
-    let promises = [];
-    shoptools.get_links(data, shop.link).forEach(x=>{
-      promises.push(
-        new Promise(async (resolve, _)=>{
-          let data = await get_data(x)
-          resolve(shoptools.parse(data, shop.link, x))
-        })
-      )
-    })
-    let res = await Promise.all(promises)
-    res = res.flat()
-    return res
-  };
-  
-  
-  module.exports = scrape
+  let promises = [];
+  shoptools.get_links(data, shop.link).forEach(x=>{
+    promises.push(
+      new Promise(async (resolve, _)=>{
+        let data = await get_data(x)
+        resolve(shoptools.parse(data, shop.link, x))
+      })
+    )
+  })
+  let res = await Promise.all(promises)
+  res = res.flat()
+  return res
+};
+
+
+module.exports = scrape
